9f5a3dc917b707643d2de43c8b59351de4715d0d
Slin <scastro@thoughtworks.com>
2017-06-22 01:44:12 +0000

    agrega control de robot por teclado

--- off.py
+++ off.py
@@ -1,4 +1,5 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
-GPIO.output(17, false)
+GPIO.setup(17,GPIO.OUT)
+GPIO.output(17, False)
--- on.py
+++ on.py
@@ -1,4 +1,7 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode(GPIO.BCM)
-GPIO.output(17, true)
+GPIO.setup(17,GPIO.OUT)
+GPIO.setup(27,GPIO.OUT)
+GPIO.output(17, True)
+GPIO.output(27,False)
