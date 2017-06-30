e475cbdb6a41c4505474f0c847db2e4f7930a031
Slin castro <scastro@thoughtworks.com>
2017-06-30 00:37:13 -0400

    add mock not working

--- DirectionControl/test_control.py
+++ DirectionControl/test_control.py
@@ -1,12 +1,13 @@
 import unittest
 from mock import MagicMock
 from control import Control
-
+from mock import patch, sentinel
 
 class TestControl(unittest.TestCase):
 
-     def test_should_configure_bcm_mode(self):
-        mock = Control()
+     @patch('control.Control')
+     def test_should_configure_bcm_mode(self,mock):
+        #mock = Control()
         mock.setmode = MagicMock()
         mock.setUpValues()
         mock.setmode.assert_called_with()
